df8967692873f62914eec4cdab185efd49eafaee
Matiss <matissj1337@gmail.com>
2023-02-15 11:30:48 +0100

    moving forward

--- 11_sanitizing_inputs.php
+++ 11_sanitizing_inputs.php
@@ -5,32 +5,29 @@
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
 
-// 1111
-// sending data and receiving that data from link or form
-// echo $_GET['name'];
 
-// 2222
-// issset check if button submit was pressed and only then echoes the data
+///1111
+// sanitizing with htmlspecialchars
+// if (isset($_POST['submit'])) {
+//     $name = htmlspecialchars($_POST['name']);
+//     $age = htmlspecialchars($_POST['age']);
+//     echo $name;
+// };
+
+///2222 sanitizing with filter_input
 if (isset($_POST['submit'])) {
-    echo $_POST['name'];
-    echo $_POST['age'];
-}
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+    echo $name;
+    echo $age;
+};
 
 
 
 
 ?>
-<!-- just a link to something -->
-<!-- <a href="<?php echo $_SERVER["PHP_SELF"]; ?>">Click</a> -->
 
-
-<!-- 1111 -->
-<!-- sending data  -->
-<!-- <a href="<?php echo $_SERVER["PHP_SELF"]; ?>?name=Matiss">Click</a> -->
-
-<!-- 2222 -->
-<!-- action is where the form is sent -->
-<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
     <div>
         <label for="name">Name:</label>
         <input type="text" name="name">
@@ -41,3 +38,8 @@ if (isset($_POST['submit'])) {
     </div>
     <input type="submit" value="Submit" name="submit">
 </form>
+
+
+<!-- 1111 -->
+<!-- can be done with htmlspecalchars -->
+<!-- <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"> -->
